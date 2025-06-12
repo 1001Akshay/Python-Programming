5f2b3cf01e10caed092a8b93774e2101e1d7ec16
1001Akshay <164662789+1001Akshay@users.noreply.github.com>
2025-06-12 22:58:36 +0530

    Update Assignment1_1.py

--- Assignment1_1.py
+++ Assignment1_1.py
@@ -1,3 +1,4 @@
+#Task 1
 a = float(input("Enter the first number: "))
 b = float(input("Enter the second number: "))
 
@@ -5,3 +6,10 @@ print("Addition: ", a+b)
 print("Subtraction: ", a-b)
 print("Multiplication: ", a*b)
 print("Division: ", a/b)
+
+
+#Task 2
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+
+print("Hello", a, b, "! Welcome to the python program.")
