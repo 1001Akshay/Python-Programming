41bac87b7bfd9808cb92d3ed617104bde73b85dd
1001Akshay <164662789+1001Akshay@users.noreply.github.com>
2025-06-29 19:36:47 +0530

    Add files via upload

---
+++ Assignment_5/Task2.py
@@ -0,0 +1,6 @@
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Original list:",l)
+Extracted = l[0:5]
+print("Extracted first five elements:",Extracted)
+Reverse = Extracted[::-1]
+print("Reversed Extracted elements:",Reverse)
