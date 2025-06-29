7a71ea3e5db5d8e8b84166a9de023997494d6b5d
1001Akshay <164662789+1001Akshay@users.noreply.github.com>
2025-06-29 19:34:25 +0530

    Delete Assignment 5 directory

--- Assignment 5/Task1.py
+++
@@ -1,6 +0,0 @@
-dict_stu = { "Alice" : 78, "Bob" : 87, "Mike" : 93, "Jack" : 67}
-name = input("Enter the student's name: ")
-if name in dict_stu:
-    print("{}'s marks: {}".format(name, dict_stu[name]))
-else:
-    print("Student not found")
--- Assignment 5/Task2.py
+++
@@ -1,6 +0,0 @@
-l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print("Original list:",l)
-Extracted = l[0:5]
-print("Extracted first five elements:",Extracted)
-Reverse = Extracted[::-1]
-print("Reversed Extracted elements:",Reverse)
